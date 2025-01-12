780053a9577f5c2baa26e01f7d79abcc4a20f1a9
robcurtis <12283105+robcurtis@users.noreply.github.com>
2025-01-11 22:55:55 -0800

    Exclude AR accounts with 0 balance

--- models/stg_quickbooks__account.sql
+++ models/stg_quickbooks__account.sql
@@ -33,32 +33,19 @@ final as (
     select
         cast(id as {{ dbt.type_string() }}) as account_id,
         cast(account_number as {{ dbt.type_string() }}) as account_number,
-        CASE WHEN 
-            (source_relation = 'quickbooks_bhdsc' and cast(account_number as {{ dbt.type_string() }}) IN ('1105', '1197', '1198', '1199')) or
-            (source_relation = 'quickbooks_bhec' and cast(account_number as {{ dbt.type_string() }}) = '1105') or
-            (source_relation = 'quickbooks_bvsc' and cast(account_number as {{ dbt.type_string() }}) IN ('1105', '1121'))
-            THEN true 
-        ELSE sub_account 
-        END as is_sub_account,
-        CASE WHEN 
-            (source_relation = 'quickbooks_bhdsc' and cast(account_number as {{ dbt.type_string() }}) IN ('1105', '1197', '1198', '1199')) or
-            (source_relation = 'quickbooks_bhec' and cast(account_number as {{ dbt.type_string() }}) = '1105') or
-            (source_relation = 'quickbooks_bvsc' and cast(account_number as {{ dbt.type_string() }}) IN ('1105', '1121')) 
-            THEN (
-                SELECT CAST(MAX(id) as {{ dbt.type_string() }})  -- Using MAX to ensure single result
-                FROM account a
-                WHERE a.source_relation = account.source_relation 
-                AND cast(a.account_number as {{ dbt.type_string() }}) = '1100'
-            )
-        ELSE cast(parent_account_id as {{ dbt.type_string() }}) 
-        END as parent_account_id,
+        is_sub_account,
+        parent_account_id,
         name,
         account_type,
         account_sub_type,
         classification,
         balance,
         balance_with_sub_accounts,
-        active as is_active,
+        CASE 
+            WHEN account_sub_type = 'AccountsReceivable' and balance != 0 and balance_with_sub_accounts != 0 
+                THEN false 
+            ELSE active 
+        END as is_active,
         created_at,
         currency_id,
         description,
@@ -66,33 +53,8 @@ final as (
         updated_at,
         source_relation,
         _fivetran_deleted 
+
     from account
-    where 
-        ((sub_account = false AND active = true) 
-        OR (
-            sub_account = true AND (
-                CASE WHEN 
-                (source_relation = 'quickbooks_bhdsc' and cast(account_number as {{ dbt.type_string() }}) IN ('1105', '1197', '1198', '1199')) or
-                (source_relation = 'quickbooks_bhec' and cast(account_number as {{ dbt.type_string() }}) IN ('1105', '1197')) or
-                (source_relation = 'quickbooks_bvsc' and cast(account_number as {{ dbt.type_string() }}) IN ('1105', '1121'))
-                THEN (
-                    SELECT CAST(MAX(id) as {{ dbt.type_string() }})  -- Using MAX to ensure single result
-                    FROM account a
-                    WHERE a.source_relation = account.source_relation 
-                    AND cast(a.account_number as {{ dbt.type_string() }}) = '1100'
-                )
-            ELSE cast(parent_account_id as {{ dbt.type_string() }}) 
-            END -- parent_account_id
-        ) in (
-            select id 
-            from account a
-            where a.active = true
-        )
-    )) AND (
-        (source_relation = 'quickbooks_bhdsc') or -- and account_number <> '1116' and account_number <> '1120') or 
-        (source_relation = 'quickbooks_bhec' and account_number <> '1116' and account_number <> '1120') or 
-        (source_relation = 'quickbooks_bvsc' and account_number <> '1110')
-    )
 )
 
 select *
